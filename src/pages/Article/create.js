c4469d9441e1c5bf2ae929438f77ae3a2c01cd24
lici <1035339896@qq.com>
2021-06-12 23:09:36 +0800

    文章发布，文本编辑器，预览，删除文章。修改文章。

--- src/pages/Article/create.js
+++ src/pages/Article/create.js
@@ -1,11 +1,197 @@
 import React, { Component } from 'react'
+// import Editor from 'for-editor'
+import BraftEditor from 'braft-editor';
+// 引入编辑器样式
+import 'braft-editor/dist/index.css'
+import { Row, Col, Button, Space, message, Select, Input, Form } from 'antd';
+import PropTypes from 'prop-types'
+import articleService from '../../service/article'
+import categoryService from '../../service/category';
+import './create.less'
 
+const { Option} = Select;
 export default class CreateArticle extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       // 创建一个空的editorState作为初始值
+      editorState: BraftEditor.createEditorState(null),
+      item: this.props.article, // 记录字段值
+      title: '', // 文章的标题
+      categoryItems: [], // 分类列表
+      category: [], // 文章的类型
+      htmlContent: '', // 文章的html内容
+      isCreate:true, // true为添加文章，false为更新操作
+    }
+    this.$form = React.createRef();
+  }
+
+  // 在组件卸载及销毁之前直接调用
+  componentWillUnmount () {
+    // 全部清空
+     this.props.removeArticle()
+  }
+
+  componentDidMount () {
+
+    categoryService.list({current:1,pageSize:10}).then(res => {
+      if (parseInt(res.code) === 0) {
+        this.setState({
+          categoryItems:res.data.items
+        })
+      }
+    })
+
+
+
+    if (this.state.item) {
+      let { content, category, title, _id } = this.state.item;
+      category = category.map(item => item._id)
+      this.setState({
+        editorState: BraftEditor.createEditorState(content),
+        title,
+        isCreate:false
+      }, () => {
+        this.$form.current.setFieldsValue({
+          title: this.state.title,
+          category,
+          id:_id
+        })
+      })
+    }
+  }
+
+  // 富文本编辑器改变的时候
+  handleChange = (editorState) => {
+    this.setState({ editorState })
+  }
+
+
+  // 提交文章 （创建/更新）
+  handleClick = () => {
+    // 获取title
+    const htmlContent = this.state.editorState.toHTML()
+    let fields = this.$form.current.getFieldsValue()
+    this.setState({
+      htmlContent
+    }, () => {
+      articleService[this.state.isCreate ? 'create':'update']({...fields,content:htmlContent}).then(res => {
+        if (parseInt(res.code) === 0) {
+          message.success(res.data)
+          this.setState({
+            value: ''
+          }, () => {
+            this.props.history.push('/admin/article')
+          })
+        } else {
+          message.error(res.error)
+        }
+      }).catch(err => {
+        message.error(err)
+      })
+    })
+  }
+
+  // 取消
+  hancleCancle = () => {
+    this.props.history.goBack();
+  }
+  
+  // 编辑器ctrl + s 自动保存内容
+  submitContent = async () => {
+    // 在编辑器获得焦点时按下ctrl+s会执行此方法
+    // 编辑器内容提交到服务端之前，可直接调用editorState.toHTML()来获取HTML格式的内容
+    const htmlContent = this.state.editorState.toHTML()
+    // const result = await saveEditorContent(htmlContent)
+    this.setState({
+      htmlContent
+    })
+  }
+
+  // category下拉框 选择
+  handleSelect = (val) => {
+    let category = this.state.category;
+    category.push(val)
+    this.setState({
+      category
+    })
+  }
+
+  // 下拉框 取消选择
+  handleDeselect = (val) => {
+    let category = this.state.category;
+    category = category.filter(item => item != val);
+    this.setState({
+      category
+    })
+  }
+  // 标题 & 分类
+  handleFieldsChange = (changedFields, allFields) => {
+    // 直接在表单操作全部表单
+    let obj = {};
+    allFields.forEach(item => {
+      obj[item.name] = item.value
+    });
+    this.setState({...obj})
+  }
+  render () {
+    const markdownOption = {
+      bold: true, // 粗体
+    }
     return (
-      <div>
-        CreateArticle
-      </div>
+      <Row className="article-edit-page">
+        <Space className="edit-button-wrapper">
+          <Button type="text" danger onClick={this.hancleCancle}>取消</Button>
+          <Button type="text" onClick={this.handleClick}>{ this.state.isCreate ? '发布':'更新'}</Button>
+          {/* <Select
+            onSelect={this.handleSelect}
+            onDeselect={this.handleDeselect}
+            bordered={false}
+            maxTagCount="3"
+            size="small"
+            mode="multiple"
+            placeholder="请选择文章类型"
+            style={{ width: 250 }}>
+            {
+              this.state.categoryItems.map(item => (
+                 <Option key={item._id} value={item._id}>{item.name}</Option>
+              ))
+            }
+          </Select> */}
+          
+          <Form layout="inline"  onFieldsChange={this.handleFieldsChange} ref={this.$form}>
+            <Form.Item label="文章标题" name="title" >
+              <Input placeholder="请输入文章标题"  bordered={false} type="text"></Input>
+            </Form.Item>
+            <Form.Item label="文章类型" name="category">
+              <Select
+                // onSelect={this.handleSelect}
+                // onDeselect={this.handleDeselect}
+                bordered={false}
+                maxTagCount="3"
+                size="small"
+                mode="multiple"
+                placeholder="请选择文章类型"
+                style={{ width: 250 }}>
+                {
+                  this.state.categoryItems.map(item => (
+                    <Option key={item._id} value={item._id}>{item.name}</Option>
+                  ))
+                }
+              </Select>
+            </Form.Item>
+            {
+              !this.state.isCreate ? <Form.Item label="" name="id" >
+                <Input type="hidden" /></Form.Item> : null
+            }
+          </Form>
+        </Space>
+
+        <Col span={24}>
+          <BraftEditor value={this.state.editorState } onChange={this.handleChange} onSave={() => { this.submitContent()}}></BraftEditor>
+        </Col>
+       </Row>
     )
   }
 }
--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react'
-import { Table, Input, Popconfirm,Modal, Button, message, Form, Row, Col } from 'antd'
+import { Table, Input, Popconfirm,Modal, Button, message, Form, Row, Col, Tag} from 'antd'
 import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons'
 import ArticleService from '../../service/article'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+import './index.less'
+//  汉化时间 貌似咩用  ~代表node_modele目录0
+moment.locale('zh-cn'); //locale 时区
 export default class Article extends Component {
   state = {
     items: [],
-    editVisible: false,
-    isCreate: true,
-    item: {}
+    pagination: {}, // 控制分页
+    selectedRowKeys: [],
+    keyword: '',
+    visible: false, // 模态框是否可见
+    articleContent: '',// 文章内容
   }
+
   componentDidMount () {
-    // this.getList();
+    this.getList();
   }
+
   getList = () => {
-    ArticleService.list().then(res => {
+    ArticleService.list({ current:this.state.pagination.current, keyword: this.state.keyword }).then(res => {
       if (parseInt(res.code) === 0) {
-        const { items } = res.data;
-        this.setState({items})
+        const { items,pageNum:current,pageSize,total} = res.data;
+        this.setState({
+          items: items.map(item => {
+            item.key = item._id;
+            return item;
+          }),
+          pagination: {
+            current,
+            pageSize,
+            total,
+            showTotal: value => `总计${value}条`,
+            showQuickJumper: true,
+            onChange:this.pageChange,
+          }
+        })
       }
     })
   }
+
+  // 搜索
+  handleSearch = (value) => {
+    this.setState({
+      keyword: value,
+      pagination: {
+        current:1
+      }
+    }, () => {
+      this.getList()
+    })
+  }
+
+
+  // 页面跳转
+  pageChange = (current) => {
+    this.setState({
+       pagination:{...this.state.pagination,current}
+    },this.getList)
+  }
+
+  // 创建文章
   create = () => {
     this.props.history.push('/admin/article/create')
     this.setState({
@@ -27,49 +72,110 @@ export default class Article extends Component {
       isCreate:true
     })
   }
+
+
+  onSelectChange = selectedRowKeys => {
+    this.setState({ selectedRowKeys });
+  }
+
+  // 确定删除
+  handleConfirm = (id) => {
+    ArticleService.remove(id).then(res => {
+      if (parseInt(res.code) === 0) {
+        console.log(res)
+        message.success(res.data);
+        this.setState({
+          pagination:{current:1}
+        }, () => {
+          this.getList();    
+        })
+        
+      } else {
+        message.success(res.error)
+      }
+    }).catch(res => {
+      message.error(res)
+    })
+  }
+
+  // preview 预览
+  handlePreview = (record) => {
+    ArticleService.addPv(record._id).then(res => {
+      if (parseInt(res.code) === 0) {
+        this.setState({
+          articleContent: record.content,
+          visible:true
+        },this.getList)
+      } else {
+        message.error(res.data)
+      }
+    })
+
+  }
+  handleCancel = () => {
+    this.setState({ visible: false })
+
+  }
+  // edit 编辑
+  handleEdit = (record) => {
+    // 提交给父组件处理
+    this.props.saveArticle(record)
+    this.props.history.push('/admin/article/create')
+  }
+
   render () {
     const columns = [
       {
         title: '标题',
+        width:'15%',
         dataIndex: 'title',
-        key:'title'
+        key: 'title',
+        ellipsis:true,
       },
       {
         title: '内容',
+        width:'15%',
+        ellipsis:true,
         dataIndex: 'content',
         key:'title'
       },
       {
         // 重点
         title: '分类',
+        width:'15%',
         dataIndex: 'category',
         key: 'category',
-        render: (text, record) => {
+        render: (text, record) => (
           // text  record 当前项的对象
           // text本来是一个字符串，但是我们会在后台处理（populate），它会变为分类对象
-          return text.name
-        }
+          text.map(item => {
+
+            let color = ['success', 'processing', 'error', 'warning', 'default']
+            let num = Math.ceil(Math.random()*color.length)
+            return <Tag key={item._id} color={color[num]}>{ item.name}</Tag>
+          })
+        )
       },
       {
         title: '阅读量',
+        width:'5%',
         dataIndex: 'pv',
         key:'pv'
       },
       {
         title: '创建时间',
-        dataIndex: 'createAt',
-        key:'createAt'
-      },
-      {
-        title: '创建时间',
+        width:'10%',
         dataIndex: 'createAt',
         key: 'createAt',
         render: (text, record) => {
-          return text.toLocaleString()
+          // format('YYYY-MM-DD HH:MM')
+          return moment(text).fromNow()
         }
       },
       {
         title: '评论数',
+        width: '5%',
+
         dataIndex: 'comments',
         key: 'comments',
         render: (text) => {
@@ -77,43 +183,84 @@ export default class Article extends Component {
         }
       },
       {
+        title: '作者',
+        width: '10%',
+        dataIndex: 'user',
+        render: (text) => {
+          return text && text.nickname
+        }
+      },
+      {
         title: '操作',
         dataIndex: 'action',
+        // width:300,
+        // fixed:'right',
         key: 'action',
-        render: () => {
+        render: (text,record) => {
           return (
-            <Button.Group>
-              <Button type="dashed">查看</Button>
-              <Button type="primary">编辑</Button>
-              <Button type="primary">评论</Button>
-              <Button type="danger">删除</Button>
-            </Button.Group>
+            <>
+              <Button type="dashed" style={{ marginLeft: 5, marginTop: 2 }} onClick={() => this.handlePreview(record)}>查看</Button>
+              <Button type="primary" style={{marginLeft:5,marginTop:2}} onClick={()=>this.handleEdit(record)}>编辑</Button>
+              <Button type="primary" style={{marginLeft:5,marginTop:2}}>评论</Button>
+              <Popconfirm
+                okText="确定"
+                cancelText="取消"
+                title="确定删除吗？"
+                onConfirm={()=>{this.handleConfirm(record._id)}}
+                onCancel={() => { }}
+              >
+                <Button type="danger"  style={{marginLeft:5,marginTop:2}}>删除</Button>
+              </Popconfirm>
+            </>
           )
         }
       }
     ]
+    const rowSelection = {
+      onChange: this.onSelectChange,
+    }
     return (
-      <Row>
-        <Col span={12}>
+      <Row className="article-page">
+        <Col span={8}>
           <Button.Group>
             <Button type="dashed" icon={<PlusCircleOutlined />} onClick={this.create}>添加文章</Button>
-            <Button type="danger" icon={<DeleteOutlined />} >删除文章</Button>
+            <Popconfirm
+              okText="确定"
+              cancelText="取消"
+              title="确定删除吗？"
+              width="800px"
+              onConfirm={()=>{this.handleConfirm(this.state.selectedRowKeys)}}
+              onCancel={() => { }}
+            >
+              <Button type="danger" icon={<DeleteOutlined />} >删除文章</Button>
+            </Popconfirm>
           </Button.Group>
         </Col>
-        <Col span={12}>
+        <Col span={8}>
           <Input.Search
             enterButton
             allowClear
+            onSearch={this.handleSearch}
             placeholder="请输入搜索内容" />
         </Col>
         <Col span={24} style={{marginTop:20}}>
-          <Table columns={columns} dataSource={this.state.items}></Table>
+          <Table
+            columns={columns}
+            bordered
+            rowSelection={rowSelection}
+            pagination={this.state.pagination}
+            dataSource={this.state.items}>
+          </Table>
           <Modal
-            title={this.state.title}
-            onCancel={this.editCancel}
-            onOk={this.editOK}
-            destroyOnClose
-          ></Modal>
+            visible={this.state.visible}
+            title="文章预览"
+            closable
+            footer={null}
+            onCancel={this.handleCancel}
+          >
+            <div dangerouslySetInnerHTML={{__html:this.state.articleContent}}>
+            </div>
+          </Modal>
         </Col>
       </Row>
     )
